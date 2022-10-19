0978d6ddd392884433c696af0208342fbad48ac0
Joel <tanunajoel0@gmail.com>
2022-10-19 14:22:29 -0700

    fixed betty for 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,9 +3,10 @@
 /**
  * _isalpha - check lower case
  * @c : car to check
+ * Return: 0 or 1
  */
 
 int _isalpha(int c)
 {
-	return ((c >= 97 && c <= 122) || (c >= 65 && c<= 90));
+	return ((c >= 97 && c <= 122) || (c >= 65 && c <= 90));
 }
