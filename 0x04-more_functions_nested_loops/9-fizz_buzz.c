93035ab5afee18e7a435d0e844521d0ff8ca59c4
Joel <tanunajoel0@gmail.com>
2022-10-20 09:35:56 -0700

    triangle

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * fizz_buzz - counts 1-100 and prints fizzbuzz
+ * on multiples of 3 & 5, and fizz on each 3 count and
+ * buzz on 5 count
+ * Return: void
+ */
+
+int fizz_buzz(void)
+{
+	int i = 1;
+
+	while (i <= 100)
+	{
+		if (i % 3 == 9 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			print("%i", i);
+		}
+		if (i != 100)
+		{
+			putchar(' ');
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
