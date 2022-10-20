874b1ce85f26c92f573c82ccf0914dcf230c7f73
Joel <tanunajoel0@gmail.com>
2022-10-20 06:40:13 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - returns product of its characters
+ * @a : int type number
+ * @b : int type number
+ * Return: 0
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
